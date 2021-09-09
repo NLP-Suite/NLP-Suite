242b4ffd8dbfbe505f7498e88472b0f40c7b0251
NLP Suite <rfranzo@emory.edu>
2021-09-09 11:23:41 -0400

    Added a check for anaconda3 in STEP2-install_NLP-Suite.command

--- src/GUI_util.py
+++ src/GUI_util.py
@@ -169,7 +169,7 @@ def display_release():
     # second digit for new features
     # third digit for bug fixes and minor changes to current version
     # must also change the Release version in readMe on GitHub
-    release_version_var.set("1.5.6")
+    release_version_var.set("1.5.7")
 
     y_multiplier_integer=-.7
 
