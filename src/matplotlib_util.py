074ae4350f4c3798e04fe5ae1d77d65cab89d4d9
Tony Chen Gu <chentony2011@hotmail.com>
2022-02-20 16:40:10 -0500

    add frequency calculation function

--- src/matplotlib_util.py
+++ src/matplotlib_util.py
@@ -10,6 +10,7 @@ import sys
 
 #import tkinter.messagebox as mb
 from collections import Counter
+from webbrowser import get
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -48,7 +49,7 @@ def prepare_data_bar(fileName, columns_to_be_plotted, hover_label):
         hover = csv.DictWriter(csvfile, fieldnames=hover_label)
     return data, hover
 
-#plot bar chart
+#plot bar chart with matplotlib
 def plot_bar_chart(x_label, height, fileName, outputDir, chartTitle):
     data = pd.read_csv(fileName, encoding='utf-8')
     plt.bar(data[x_label], data[height], align='center', alpha=0.5)
@@ -58,6 +59,17 @@ def plot_bar_chart(x_label, height, fileName, outputDir, chartTitle):
     plt.show()
     return
 
+# get frequencies of categorical variables
+def get_frequencies(data, variable):
+    #this line give a column of the counts for the categorical vairable
+    #the name of row is the categorical variable
+    data_count = data[variable].value_counts()
+    #extract the row name = categorical variables
+    data_head = data_count.index
+    header = variable+"_count"
+    #rebuild the dataframe with a column of categorical vairables and a column of their counts
+    #the row name is still the categorical variable
+    return pd.DataFrame({variable:data_head,header:data_count})
 
 #plot bar chart with plotly
 #fileName is a csv file with data to be plotted 
@@ -65,13 +77,18 @@ def plot_bar_chart(x_label, height, fileName, outputDir, chartTitle):
 #height indicates the column name of y axis from the data
 #the output file would be a html file with hover over effect names by the chart title
 #duplicates allowed, would add up the counts
-def plot_bar_chart_px(x_label, height, fileName, outputDir, chartTitle):
+#Users are expected to provide the x label and their hights.
+#If not call the get_frequencies function to get the frequencies of the categorical variables in x_label column
+def plot_bar_chart_px(x_label, fileName, outputDir, chartTitle, height = None):
     data = pd.read_csv(fileName, encoding='utf-8')
-    fig = px.bar(data, x=x_label, y=height)
+    if height is None:
+        height = x_label+"_count"
+        data = get_frequencies(data, x_label)
+    fig = px.bar(data,x=x_label,y=height)
     fig.update_layout(title=chartTitle)
     fig.show()
     savepath = os.path.join(outputDir, chartTitle + '.html')
-    #fig.write_html(savepath)
+    fig.write_html(savepath)
     return
 
 #plot pie chart with plotly
@@ -80,7 +97,7 @@ def plot_bar_chart_px(x_label, height, fileName, outputDir, chartTitle):
 #height indicates the column name of y axis from the data
 #the output file would be a html file with hover over effect names by the chart title
 #duplicates allowed, would add up the counts
-def plot_pie_chart_px(x_label, height, fileName, outputDir, chartTitle):
+def plot_pie_chart_px(x_label, fileName, outputDir, chartTitle, height = None):
     data = pd.read_csv(fileName, encoding='utf-8')
     fig = px.pie(data, values=height, names=x_label)
     fig.update_layout(title=chartTitle)
@@ -117,6 +134,7 @@ def plot_radar_chart_px(theta_label, r_label, fileName, outputDir, chartTitle):
     #fig.write_html(savepath)
     return
 
+#=======================================================================================================================
 #debug use
 def main():
     x_label = 'char'
@@ -126,7 +144,7 @@ def main():
     fileName =  'C:/Users/Tony Chen/Desktop/NLP_working/Test Input/bar_test_data.csv'
     outputDir = 'D:/'
     #plot_bar_chart(x_label, height, fileName, outputDir, chartTitle, hover_label)
-    plot_radar_chart_px(x_label, height, fileName, outputDir, chartTitle)
+    plot_bar_chart_px(x_label,fileName, outputDir, chartTitle, height = height)
 
 if __name__ == "__main__":
     main()
